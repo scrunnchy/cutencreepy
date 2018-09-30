3fab406f31077ae69f02fa58e6aea14fbe71ff9a
Kindra Lingenfelter <31263945+scrunnchy@users.noreply.github.com>
2018-09-30 17:51:09 -0600

    added level manager script

---
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelManager : MonoBehaviour {
+
+    UnityEvent conductor = new UnityEvent();
+
+	// Use this for initialization
+	void Start () {
+        conductor.AddListener(enemyPlayerCollision);
+        conductor.AddListener(checkpointCollision);
+        conductor.AddListener(composerEvent);
+	}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void enemyPlayerCollision()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void composerEvent()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void checkpointCollision()
+    {
+        throw new NotImplementedException();
+    }
+}
