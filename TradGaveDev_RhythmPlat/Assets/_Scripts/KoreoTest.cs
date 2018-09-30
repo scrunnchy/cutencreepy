8987aee9114922c5e45a0ec6db2ae4b3e245b98b
Bryson Oar <bryson.oar@hotmail.com>
2018-09-30 13:38:59 -0600

    Scripts Folder
    Scripts folder contains a script that implements basic Koreo stuff

---
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/KoreoTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class KoreoTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Koreographer.Instance.RegisterForEvents("eventIDHere", activateThisMethod);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void activateThisMethod (KoreographyEvent evt)
+    {
+
+    }
+}
