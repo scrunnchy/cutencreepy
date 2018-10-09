643cb6f0bbb5b5cead1c4685bec1cc43387e07a5
Kindra Lingenfelter <31263945+scrunnchy@users.noreply.github.com>
2018-10-09 16:33:21 -0600

    pause menu is fully functional :)

---
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public LevelManager LM;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkForKeyStroke();
+    }
+
+    private void checkForKeyStroke()
+    {
+        if (Input.GetButtonDown("pauseButton"))
+            LM.togglePause();
+    }
+}
