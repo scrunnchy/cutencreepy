7c8b6d3fba12242a51120d4fb79b2323e46f03b0
Bryson Oar <bryson.oar@hotmail.com>
2018-10-10 01:23:43 -0600

    Jump Height/ Gravity Testing added to BrysonScene
    I made it so player's jump follows the "better jumping" format used in some games. Demo of it is within BrysonScene

--- TradGaveDev_RhythmPlat/Assets/Class Prototype/Scripts/Platformer/PlatformerCharacter_DirectTesting.cs
+++ TradGaveDev_RhythmPlat/Assets/Class Prototype/Scripts/Platformer/PlatformerCharacter_DirectTesting.cs
@@ -19,7 +19,6 @@ namespace Platformer
         public string horizontalAxis = "Horizontal";
         public string verticalAxis = "Vertical";
         public string jumpAxis = "Jump";
-        public string attackAxis = "Fire1";
 
         [Header("Movment Properties")]
         // Unity meters 10 : 12.2 (Track 1)
@@ -33,14 +32,12 @@ namespace Platformer
 
         [Header("Jump Properties")]
         public float jumpForce = 3f;
+        public float fallMultiplier = 2.5f;
+        public float lowJumpMultiplier = 2f;
         [Range(0f, 1f)]
         public float airControl = 0.85f;
         public float gravityModifier = 2.5f;
         public float terminalVelocity = 25f;
-
-        [Header("Attack Properties")]
-        public Weapon primaryAttack;
-        public Transform attackPoint;
 
 
         //Private Memeber Variables
@@ -68,7 +65,8 @@ namespace Platformer
 
         private void Update()
         {
-            Debug.Log(_characterVelocity);
+            Debug.Log(Input.GetAxis(jumpAxis));
+            //Debug.Log(_characterVelocity);
             bool isGrounded = Grounded();
 
             if (_canMove)
@@ -92,32 +90,27 @@ namespace Platformer
             // If the character is in the air: apply gravity, reduce force by air control
             if (!isGrounded)
             {
+                /*if (_characterVelocity.y > 0 && _characterVelocity.y < .5)
+                {
+
+                }*/
+                if (_characterVelocity.y < .5)
+                {
+                    _characterVelocity += Vector3.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+                }
                 // If we haven't reached terminal velocity, apply gravity
                 if (_characterVelocity.y > -terminalVelocity)
                 {
                     _characterVelocity.y += gravityModifier * Physics.gravity.y * Time.deltaTime;
                 }
-
-                _force.x *= airControl;
-            }
-
-            if (_inJump)
-            {
-                _characterVelocity.x += _jumpMomentum;
             }
 
             _force *= massCoeficcient;
             _characterVelocity += _force;
-            _characterVelocity.x *= frictionCoefficient;
-            /*if (_characterVelocity.x < maxSpeed)
-            {
-                _characterController.Move((_characterVelocity) * Time.deltaTime);
-            }*/
             _characterController.Move((_characterVelocity) * Time.deltaTime);
-            // Orients the player toward the character velocity direction
-            //if (_canMove) Orient();
 
-            if (_canAttack) Attack();
+            // Orients the player toward the character velocity direction -- commented for testing purposes
+            //if (_canMove) Orient();
 
         }
 
@@ -125,8 +118,7 @@ namespace Platformer
         {
             if (Mathf.Abs(_characterVelocity.x) < maxSpeed)
             {
-                //_force.x = Input.GetAxis(horizontalAxis) * acceleration;
-                _force.x = maxSpeed;
+                _characterVelocity.x = maxSpeed;
             }
         }
 
@@ -138,14 +130,26 @@ namespace Platformer
                 _characterVelocity.y = 0f;
                 _force.y = jumpForce;
 
-                // Set jump momentum in relation to air control, keeps the player moving after leaving the ground if air control is low
-                _jumpMomentum = _force.x * ((airControl - 1f) * -1f);
-
                 _inJump = true;
                 _canJump = false;
             }
+            //Debug.Log(_characterVelocity.y);
+            if (_inJump)
+            {
+                /*if (_characterVelocity.y < 0)
+                {
+                    //Debug.Log("Falling");
+                    _characterVelocity += Vector3.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+                }
+                else if (_characterVelocity.y > 0 && !(Input.GetAxis(jumpAxis) > 0f))
+                {
+                    Debug.Log("Low Jumping");
+                    _characterVelocity += Vector3.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+                }*/
+            }
         }
 
+        /*
         private void Orient()
         {
             Vector3 orientation = Vector3.zero;
@@ -153,7 +157,7 @@ namespace Platformer
             orientation.x = _characterVelocity.x;
 
             if (orientation != Vector3.zero) this.transform.forward = orientation;
-        }
+        }*/
 
         private bool Grounded()
         {
@@ -162,34 +166,13 @@ namespace Platformer
             _inJump = !controllerGrounded;
 
             return controllerGrounded;
-
-
-            // NOTE: "isGrounded" has been unreliable in the past, use raycast or Physics.CheckSphere if it becomes an issue
-
-            //RaycastHit raycastHit;
-
-            //if (Physics.Raycast(this.transform.position, Vector3.down, out raycastHit, 1.2f))
-            //{
-            //    if (raycastHit.collider.gameObject != this.gameObject)
-            //    {
-            //        _characterVelocity.y = 0f;
-            //        _inJump = false;
-            //        return true; 
-            //    }
-            //}
         }
 
 
-        private void Attack()
-        {
-            if (Input.GetAxis(attackAxis) > 0.5f)
-            {
-                primaryAttack.Fire(attackPoint);
-            }
-        }
 
         /// <summary>
         /// Freeze the character in place, store the current character velocity, or unfreeze the character and resume character velocity.
+        /// Called when pause screen is called
         /// </summary>
         /// <param name="value">If set to <c>true</c> value.</param>
         public void Freeze(bool value)
