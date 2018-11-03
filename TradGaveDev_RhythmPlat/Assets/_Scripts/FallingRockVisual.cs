1a6cf51e2cb931ef55502029bacba2b4b127bffc
Bryson Oar <bryson.oar@hotmail.com>
2018-11-03 17:27:38 -0600

    Falling rock implementation and better reversal animations
    removed exit times for reversal animations and improved the falling rock and made a prefab for it.

--- TradGaveDev_RhythmPlat/Assets/_Scripts/FallingRockVisual.cs
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/FallingRockVisual.cs
@@ -9,65 +9,70 @@ public class FallingRockVisual : MonoBehaviour {
     Vector3 playerPos;
     // Track if in reversal state
     private bool isReversed;
-
+    // Track if rock is currently falling
+    private bool isFalling;
+    // If rock is expended, it will not trigger again until reversal is done
     private bool isExpended;
-    //rock height at reset
-    int initialRockHeight = 6;
-    //x distance before activation
-    float rockTriggerDistance = 4.4608f - .9f;
+    // Distance at which rock will be triggered to fall
+    public float rockTriggerDistance = 6f;
     // rigidbody of rock
-    Rigidbody rb;
-
-    float rockResetDelay = 6f;
+    private Rigidbody rb;
+    // delay in which the rock's position gets reset to its original position
+    float rockResetDelay = 2f;
 
     // Use this for initialization
     void Start ()
     {
+        // Get the position we will reset the rock to later
         originalRockPos = gameObject.transform.position;
-        Rigidbody rb = this.GetComponent<Rigidbody>();
-        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        rb = gameObject.GetComponent<Rigidbody>();
+        // If reversal is active we want to flip the rock's trigger point
+        Checkpoint.CheckpointReverse.AddListener(flipRockOrientation);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        // Constantly update playe rposition so rock can accurately tell if it should fall
         playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
-        Debug.Log(this.transform.position.x - playerPos.x);
-        if (!isReversed) {
+        // We go into the if statement if we are not in reversal and we can still make the rock fall
+        if (!isReversed && !isExpended) {
             if ((this.transform.position.x - playerPos.x) < rockTriggerDistance)
             {
+                isExpended = true;
+                isFalling = true;
                 rb.useGravity = true;
             }
         }
+        // else we are either in reversal or rock is expended. 
         else {
-            if ((playerPos.x - this.transform.position.x) < rockTriggerDistance)
+            if ((playerPos.x - this.transform.position.x) < rockTriggerDistance +.1 && !isExpended)
             {
+                isExpended = true;
+                isFalling = true;
                 rb.useGravity = true;
             }
         }
-       
-    }
-    private bool isFalling()
-    {
-        return false;
-    }
-    private bool Grounded()
-    {
-        RaycastHit raycastHit;
-        if (Physics.Raycast(this.transform.position, Vector3.down, out raycastHit, 1.2f))
+        // if statement executed when the rock is falling. We have the second part of the statement so 
+        // that we don't "reset rock" when the rock hasn't begun to fall yet.
+        if (isFalling && this.transform.position.y < playerPos.y)
         {
-            if (raycastHit.collider.gameObject != this.gameObject)
-            { 
-                return true;
+            if (rb.velocity.y == 0)
+            {
+                isFalling = false;
+                StartCoroutine("ResetRock");
             }
         }
-        return false;
     }
-    IEnumerator resetRock()
+    private void flipRockOrientation()
+    {
+        isExpended = false;
+        isReversed = true;
+    }
+    IEnumerator ResetRock()
     {
         yield return new WaitForSeconds(rockResetDelay);
         this.transform.position = originalRockPos;
         rb.useGravity = false;
-        isExpended = false;
     }
 }
