1d59d250b048bbd0a0a36d603b06642230393657
Kindra Lingenfelter <31263945+scrunnchy@users.noreply.github.com>
2018-11-03 13:55:15 -0600

    spike created, not yet a prefab

--- TradGaveDev_RhythmPlat/Assets/_Scripts/GroundSpikes.cs
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/GroundSpikes.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,6 +12,10 @@ public class GroundSpikes : MonoBehaviour {
     public bool DisapearsAtReverse;
     //Produces the behaviour that a falling rock will only become visible and a threat during reversals
     public bool AppearsAtReverse;
+    //The speed at which the spikes will move up.
+    public float speed = 1f;
+    //The amount of units the spikes will move up
+    public float amountUp = 7f;
 
     //create event for damage "enemyPlayerCollision"
     public static UnityEvent enemyPlayerCollision;
@@ -80,6 +85,7 @@ public class GroundSpikes : MonoBehaviour {
     {
         if (!isExpended && collider.gameObject.tag == "Player")
         {
+            moveSprite();
             PlayerControl playerInfo = collider.gameObject.GetComponent<PlayerControl>();
             // check if the layer is in any valid avoid state.
             if (!playerInfo._inJump)
@@ -91,5 +97,13 @@ public class GroundSpikes : MonoBehaviour {
             }
         }
         
+    }
+
+    private void moveSprite()
+    {
+        SpriteRenderer sprite = GetComponentInChildren<SpriteRenderer>();
+        float step = speed * Time.deltaTime;
+        Vector3 up = new Vector3(sprite.transform.position.x, sprite.transform.position.y + amountUp, sprite.transform.position.z);
+        sprite.transform.position = Vector3.Lerp(transform.position, up, step);   
     }
 }
