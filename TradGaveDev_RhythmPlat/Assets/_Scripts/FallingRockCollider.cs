46f6a3ba3601f31ac2c0edd35a2d2b70f1e35738
Bryson Oar <bryson.oar@hotmail.com>
2018-11-03 13:30:59 -0600

    Fixed falling rock script causing compile errors.
    deleted the script and moved its contents over to falling rock collider

--- TradGaveDev_RhythmPlat/Assets/_Scripts/FallingRockCollider.cs
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/FallingRockCollider.cs
@@ -46,4 +46,64 @@ public class FallingRockCollider : MonoBehaviour {
         }
 
     }
+    /*
+     * CODE FROM FALLING ROCK BEFORE 
+ Rigidbody rb;
+
+ // Use this for initialization
+ void Start () {
+     rb = GetComponent<Rigidbody>();
+     spriteR = GetComponent<SpriteRenderer>();
+     boxC = GetComponent<BoxCollider>();
+
+     if (enemyPlayerCollision == null)
+     {
+         enemyPlayerCollision = new UnityEvent();
+     }
+
+     //Register for reversal.
+     Checkpoint.CheckpointReverse.AddListener(ChangeAppearanceOnReverse);
+
+     //disappear if enemy is to appear during reversals
+     if (AppearsAtReverse)
+     {
+         boxC.enabled = false;
+         spriteR.enabled = false;
+     }
+ }
+ /// <summary>
+ /// triggers on reversal events
+ /// enables/disables this obstacle's box collider and sprite renderer based on settings.
+ /// </summary>
+ private void ChangeAppearanceOnReverse()
+ {
+     //appear if invisable and dissapear if set to toggle upon reversal.
+     if (AppearsAtReverse || DisapearsAtReverse)
+     {
+         if (spriteR.enabled)
+         {
+             boxC.enabled = false;
+             spriteR.enabled = false;
+         }
+         else
+         {
+             boxC.enabled = true;
+             spriteR.enabled = true;
+         }
+     }
+ }
+
+ private void OnTriggerEnter(Collider other)
+ {
+     if (other.gameObject.name.Equals("Player"))
+     {
+         rb.isKinematic = false; 
+     }
+ }
+
+ // Update is called once per frame
+ void Update () {
+
+ }
+ */
 }
--- TradGaveDev_RhythmPlat/Assets/_Scripts/FallingRockVisual.cs
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/FallingRockVisual.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class FallingRockVisual : MonoBehaviour {
 
+    Vector3 originalRockPos;
+
     Vector3 playerPos;
     // Track if in reversal state
     private bool isReversed;
+
+    private bool isExpended;
     //rock height at reset
     int initialRockHeight = 6;
     //x distance before activation
@@ -14,27 +18,56 @@ public class FallingRockVisual : MonoBehaviour {
     // rigidbody of rock
     Rigidbody rb;
 
+    float rockResetDelay = 6f;
+
     // Use this for initialization
     void Start ()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
+        originalRockPos = gameObject.transform.position;
+        Rigidbody rb = this.GetComponent<Rigidbody>();
         playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Debug.Log(this.transform.position.x - playerPos.x);
         if (!isReversed) {
             if ((this.transform.position.x - playerPos.x) < rockTriggerDistance)
             {
-                
+                rb.useGravity = true;
             }
         }
         else {
             if ((playerPos.x - this.transform.position.x) < rockTriggerDistance)
             {
-
+                rb.useGravity = true;
             }
         }
+       
+    }
+    private bool isFalling()
+    {
+        return false;
+    }
+    private bool Grounded()
+    {
+        RaycastHit raycastHit;
+        if (Physics.Raycast(this.transform.position, Vector3.down, out raycastHit, 1.2f))
+        {
+            if (raycastHit.collider.gameObject != this.gameObject)
+            { 
+                return true;
+            }
+        }
+        return false;
+    }
+    IEnumerator resetRock()
+    {
+        yield return new WaitForSeconds(rockResetDelay);
+        this.transform.position = originalRockPos;
+        rb.useGravity = false;
+        isExpended = false;
     }
 }
