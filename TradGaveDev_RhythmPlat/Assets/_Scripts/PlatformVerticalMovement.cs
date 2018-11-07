d105cb9031f233e0e428cd77e3d089d198e020b8
Bryson Oar <bryson.oar@hotmail.com>
2018-11-06 20:17:47 -0700

    VerticalMovement prefab is also now very fab.
    Updated code for platform that moves vertically and new prefab for this platform.

--- TradGaveDev_RhythmPlat/Assets/_Scripts/PlatformVerticalMovement.cs
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/PlatformVerticalMovement.cs
@@ -5,17 +5,14 @@ using SonicBloom.Koreo;
 
 public class PlatformVerticalMovement : MonoBehaviour {
 
-    // Transforms to act as start and end markers for the journey.
-    public Vector3 startMarker;
-    public Vector3 endMarker;
+    // Vector3's to act as start and end markers for the journey.
+    private Vector3 startMarker;
+    private Vector3 endMarker;
 
     public bool moveUpwards;
     public bool moveDownwards;
 
-    private bool isOriginalHeight;
-
     private Vector3 platformOriginalHeight;
-    public float lerpTime;
 
     // Movement speed in units/sec.
     public float speed = 1.0F;
@@ -25,24 +22,37 @@ public class PlatformVerticalMovement : MonoBehaviour {
 
     // Total distance between the markers.
     private float journeyLength;
+    
+    // array that will hold the different positions the platform will go to
+    private Vector3[] points;
 
-    public GameObject[] waypoints;
+    // amount of meters we want to offset the positions
+    public float yOffset = 0;
 
 	// Use this for initialization
 	void Start ()
     {
+        // initialize point array we will use to contain positions 
+        points = new Vector3[10];
+        // top platform position
+        points[1] = new Vector3(transform.position.x, transform.position.y + yOffset, transform.position.z);
+        // bottom platform position
+        points[0] = new Vector3(transform.position.x, transform.position.y - yOffset, transform.position.z);
+        // get the platform's original height/position
         platformOriginalHeight = this.transform.position;
+        // register for koreography
         Koreographer.Instance.RegisterForEvents("SingleBeatTrack", movePlatform);
+
         // Keep a note of the time the movement started.
         startTime = Time.time;
-
-        //lerpTime = 60 / (float)Koreographer.Instance.GetMusicBPM();
+        
         // Calculate the journey length.
         journeyLength = Vector3.Distance(startMarker, endMarker);
     }
 	
 	// Update is called once per frame
-	void Update() {
+	void Update()
+    {
 
         journeyLength = Vector3.Distance(startMarker, endMarker);
         //Debug.Log(journeyLength);
@@ -58,162 +68,61 @@ public class PlatformVerticalMovement : MonoBehaviour {
     void movePlatform(KoreographyEvent evt)
     {
         //Debug.Log("move platform entered");
-         if (moveUpwards)
+
+        // If we go into this code, we are either currently at bottom or regular platform positions
+        if (moveUpwards)
          {
              if ((platformOriginalHeight.y - transform.position.y) < .5)
              {
-                 Debug.Log("move platform part 1");
                  // move from platform original position to top position
-                 //transform.position = Vector3.Lerp(platformOriginalHeight, waypoints[1].transform.position, (lerpTime));
+
+                 // start at original platform position
                  startMarker = platformOriginalHeight;
-                 endMarker = waypoints[1].transform.position;
+                // end at top position
+                 endMarker = points[1];
                  startTime = Time.time;
+                // Now that we are at the top, set values so we can go back down
                  moveUpwards = false;
                  moveDownwards = true;
              }
              else
              {
-                 Debug.Log("move platform part 2");
+                // move from bottom position to platform original position
+
+                // end at regular platform height
                  endMarker = platformOriginalHeight;
-                 startMarker = waypoints[0].transform.position;
+                // start at bottom position
+                 startMarker = points[0];
                  startTime = Time.time;
-                 // move from bottom position to platform original position
-                 //transform.position = Vector3.Lerp(waypoints[1].transform.position, platformOriginalHeight, (lerpTime));
              }
          }
+         // If we go into this code, we are either currently at top or regular platform positions
          else if (moveDownwards)
          {
              if ((transform.position.y - platformOriginalHeight.y) < .5)
              {
-                 Debug.Log("move platform part 3");
-                 //transform.position = Vector3.Lerp(platformOriginalHeight, waypoints[0].transform.position, (lerpTime));
-                 // move from platform position to bottom position
+                // move from original platform position to bottom position
+
+                // start at original platform position
                  startMarker = platformOriginalHeight;
-                 endMarker = waypoints[0].transform.position;
+                // end at bottom platform position
+                endMarker = points[0];
                  startTime = Time.time;
-                 moveUpwards = true;
+
+                // Now that we are at the bottom, set values so we can go back up
+                moveUpwards = true;
                  moveDownwards = false;
              }
              else
              {
-                // move from bottom position to platform position
-                 endMarker = platformOriginalHeight;
-                 startMarker = waypoints[1].transform.position;
+                // move from top position to original platform position
+
+                // end at original platform position
+                endMarker = platformOriginalHeight;
+                // start at top platform position
+                startMarker = points[1];
                  startTime = Time.time;
-                 Debug.Log("move platform part 4");
-                 //transform.position = Vector3.Lerp(waypoints[0].transform.position, platformOriginalHeight, (lerpTime));
              }
          }
     }
 }
-
-
-
-
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SonicBloom.Koreo;
-
-public class PlatformVerticalMovement : MonoBehaviour
-{
-
-    // Transforms to act as start and end markers for the journey.
-    public Transform startMarker;
-    public Transform endMarker;
-
-    public bool moveUpwards;
-    public bool moveDownwards;
-
-    private bool isOriginalHeight;
-
-    private Transform platformOriginalHeight;
-    public float lerpTime;
-
-    // Movement speed in units/sec.
-    public float speed = 1.0F;
-
-    // Time when the movement started.
-    private float startTime;
-
-    // Total distance between the markers.
-    private float journeyLength;
-
-    public GameObject[] waypoints;
-
-    // Use this for initialization
-    void Start()
-    {
-        platformOriginalHeight = this.transform;
-        Koreographer.Instance.RegisterForEvents("EveryOtherBeatTrack", movePlatform);
-        // Keep a note of the time the movement started.
-        startTime = Time.time;
-
-        //lerpTime = 60 / (float)Koreographer.Instance.GetMusicBPM();
-        // Calculate the journey length.
-        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Distance moved = time * speed.
-        float distCovered = (Time.time - startTime) * speed;
-        // Fraction of journey completed = current distance divided by total distance.
-        float fracJourney = distCovered / journeyLength;
-        // Set our position as a fraction of the distance between the markers.
-        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
-    }
-
-    void movePlatform(KoreographyEvent evt)
-    {
-        //Debug.Log("move platform entered");
-        if (moveUpwards)
-        {
-            if ((platformOriginalHeight.position.y - transform.position.y) < .5)
-            {
-                Debug.Log("move platform part 1");
-                // move from platform original position to top position
-                //transform.position = Vector3.Lerp(platformOriginalHeight, waypoints[1].transform.position, (lerpTime));
-                startMarker = platformOriginalHeight;
-                endMarker = waypoints[1].transform;
-                startTime = Time.time;
-                moveUpwards = false;
-                moveDownwards = true;
-            }
-            else
-            {
-                Debug.Log("move platform part 2");
-                endMarker = platformOriginalHeight;
-                startMarker = waypoints[1].transform;
-                startTime = Time.time;
-                // move from bottom position to platform original position
-                //transform.position = Vector3.Lerp(waypoints[1].transform.position, platformOriginalHeight, (lerpTime));
-            }
-        }
-        else if (moveDownwards)
-        {
-            if ((transform.position.y - platformOriginalHeight.position.y) < .5)
-            {
-                Debug.Log("move platform part 3");
-                //transform.position = Vector3.Lerp(platformOriginalHeight, waypoints[0].transform.position, (lerpTime));
-                startMarker = platformOriginalHeight;
-                endMarker = waypoints[0].transform;
-                startTime = Time.time;
-                moveUpwards = true;
-                moveDownwards = false;
-            }
-            else
-            {
-                endMarker = platformOriginalHeight;
-                startMarker = waypoints[0].transform;
-                startTime = Time.time;
-                Debug.Log("move platform part 4");
-                //transform.position = Vector3.Lerp(waypoints[0].transform.position, platformOriginalHeight, (lerpTime));
-            }
-        }
-    }
-}
-*/
