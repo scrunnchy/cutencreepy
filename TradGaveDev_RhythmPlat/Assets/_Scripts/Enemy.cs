a95bc4e1b5d45e9ee26893402c732a306aadf3ba
Bryson Oar <bryson.oar@hotmail.com>
2018-11-14 13:20:14 -0700

    attempt to revert so must push everything else before revert.

--- TradGaveDev_RhythmPlat/Assets/_Scripts/Enemy.cs
+++ TradGaveDev_RhythmPlat/Assets/_Scripts/Enemy.cs
@@ -36,6 +36,7 @@ public class Enemy : MonoBehaviour {
         spriteR = GetComponent<SpriteRenderer>();
         boxC = GetComponent<BoxCollider>();
         animator = this.GetComponent<Animator>();
+        isReversed = false;
         animator.SetBool("Reversed", isReversed);
     }
     // On start, an enemy will:
@@ -147,7 +148,7 @@ public class Enemy : MonoBehaviour {
             if (gotHit)
             {
                 // trigger Damage event
-                LevelManager.enemyPlayerCollision.Invoke();
+                //LevelManager.enemyPlayerCollision.Invoke();
                 isExpended = true;
             }
         }
